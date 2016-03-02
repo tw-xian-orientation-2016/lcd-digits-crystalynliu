a1c910d66c9a86cbffcf75480ce7630c7a660d81
crystalynliu <crystalynliu@163.com>
2016-03-02 16:06:29 +0800

    add second task test to task-test.js

--- lcd-digits/test/task-test.js
+++ lcd-digits/test/task-test.js
@@ -16,3 +16,22 @@ describe('split()', function() {
   });
 
 })
+
+describe('translate()', function() {
+
+  it('correct result when have one digit', function() {
+    var result = translate(["9"]);
+    expect(result).toEqual(["._.\n","|_|\n","..|\n"]);
+  });
+
+  it('correct result when have two digit', function() {
+    var result = translate(["9","1"]);
+    expect(result).toEqual(["._. ...\n","|_| ..|\n","..| ..|\n"]);
+  });
+
+  it('correct result when have three digit', function() {
+    var result = translate(["9","1","0"]);
+    expect(result).toEqual(["._. ... ._.\n","|_| ..| |.|\n","..| ..| |_|\n"]);
+  });
+
+})
