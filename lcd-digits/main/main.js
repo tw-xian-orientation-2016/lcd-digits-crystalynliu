48eebef0c83442c92f21661a62ea36722d84e9e8
crystalynliu <crystalynliu@163.com>
2016-03-02 16:33:49 +0800

    add function translate and pass test

--- lcd-digits/main/main.js
+++ lcd-digits/main/main.js
@@ -2,3 +2,22 @@ function split(number) {
   var digits = number.split('');
   return digits;
 }
+
+function translate(digits,strings){
+  var stringSet = [];
+  var firststring="";
+  var secondString="";
+  var thridString="";
+
+  digits.forEach(function(digit){
+    firststring += strings[digit][0] + " ";
+    secondString += strings[digit][1]+ " ";
+    thridString += strings[digit][2]+" ";
+  })
+  stringSet.push(firststring,secondString,thridString);
+
+  return stringSet.map(function(string){
+    return string.substring(0,string.length-1)+"\n";
+  })
+
+}
