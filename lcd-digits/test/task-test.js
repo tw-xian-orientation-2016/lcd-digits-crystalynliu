650039c9dbe8018eabf327936719d0a059d3c505
crystalynliu <crystalynliu@163.com>
2016-03-02 16:08:38 +0800

    modify second task test

--- lcd-digits/test/task-test.js
+++ lcd-digits/test/task-test.js
@@ -18,19 +18,23 @@ describe('split()', function() {
 })
 
 describe('translate()', function() {
+  var strings;
 
+  beforeEach(function(){
+    strings = loadDigitsString();
+  })
   it('correct result when have one digit', function() {
-    var result = translate(["9"]);
+    var result = translate(["9"],strings);
     expect(result).toEqual(["._.\n","|_|\n","..|\n"]);
   });
 
   it('correct result when have two digit', function() {
-    var result = translate(["9","1"]);
+    var result = translate(["9","1"],strings);
     expect(result).toEqual(["._. ...\n","|_| ..|\n","..| ..|\n"]);
   });
 
   it('correct result when have three digit', function() {
-    var result = translate(["9","1","0"]);
+    var result = translate(["9","1","0"],strings);
     expect(result).toEqual(["._. ... ._.\n","|_| ..| |.|\n","..| ..| |_|\n"]);
   });
 
