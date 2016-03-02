3d39955e83273df234321909411d6aee263c9393
crystalynliu <crystalynliu@163.com>
2016-03-02 16:46:09 +0800

    modify bug

--- lcd-digits/test/task-test.js
+++ lcd-digits/test/task-test.js
@@ -23,7 +23,7 @@ describe('translate()', function() {
   beforeEach(function(){
     strings = loadDigitsString();
   })
-  
+
   it('correct result when have one digit', function() {
     var result = translate(["9"],strings);
     expect(result).toEqual(["._.\n","|_|\n","..|\n"]);
@@ -37,6 +37,30 @@ describe('translate()', function() {
   it('correct result when have three digit', function() {
     var result = translate(["9","1","0"],strings);
     expect(result).toEqual(["._. ... ._.\n","|_| ..| |.|\n","..| ..| |_|\n"]);
+  });
+
+})
+
+describe('printDigit()', function() {
+  var sign;
+
+  beforeEach(function(){
+    sign = loadSign();
+  })
+  
+  it('correct result when have one string', function() {
+    var result = printDigit(["._.\n","|_|\n","..|\n"],sign);
+    expect(result).toEqual("```\n._.\n|_|\n..|\n```\n");
+  });
+
+  it('correct result when have two digit', function() {
+    var result = printDigit(["._. ...\n","|_| ..|\n","..| ..|\n"],sign);
+    expect(result).toEqual("```\n._. ...\n|_| ..|\n..| ..|\n```\n");
+  });
+
+  it('correct result when have three digit', function() {
+    var result = printDigit(["._. ... ._.\n","|_| ..| |.|\n","..| ..| |_|\n"],sign);
+    expect(result).toEqual("```\n._. ... ._.\n|_| ..| |.|\n..| ..| |_|\n```\n");
   });
 
 })
