3035046ff8d4e455d1d27d90d39cf66a5e5e50bc
crystalynliu <crystalynliu@163.com>
2016-03-02 17:14:07 +0800

    refactor translate

--- lcd-digits/main/main.js
+++ lcd-digits/main/main.js
@@ -5,21 +5,17 @@ function split(number) {
 
 function translate(digits,strings){
   var stringSet = [];
-  var firststring="";
-  var secondString="";
-  var thridString="";
 
-  digits.forEach(function(digit){
-    firststring += strings[digit][0] + " ";
-    secondString += strings[digit][1]+ " ";
-    thridString += strings[digit][2]+" ";
-  })
-  stringSet.push(firststring,secondString,thridString);
-
-  return stringSet.map(function(string){
-    return string.substring(0,string.length-1)+"\n";
-  })
-
+  for(var i = 0;i<3;i++){
+    var string="";
+    digits.forEach(function(digit){
+      string += strings[digit][i] + " ";
+    })
+    string = string.substring(0,string.length-1)+"\n";
+    stringSet.push(string);
+  }
+  
+  return stringSet;
 }
 
 function printDigit(stringSet,sign){
